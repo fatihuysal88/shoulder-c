9c293196a46a3b1645a1c8f51be98cf3a781ea5b
Fatih UYSAL <61452333+fatihuysal88@users.noreply.github.com>
2021-01-21 22:10:26 +0300

    Update get_metrics.py

--- get_metrics.py
+++ get_metrics.py
@@ -14,12 +14,12 @@ from sklearn.metrics import classification_report
 
 class get_metric():
 
-    def get_accuracy_graph(self, epochs, train_acc, val_acc):  # draw validation and train accuracy graphs
+    def get_accuracy_graph(epochs, train_acc, val_acc):  # draw validation and train accuracy graphs
         plt.plot(epochs, train_acc, color='#006BA4')
         plt.plot(epochs, val_acc, color='#FF800E')
         plt.grid(b=True, which='major', color='lightgray')
         plt.grid(b=True, which='minor', color='lightgray')
-        plt.xticks(np.arange(0, 35, 5))
+        plt.xticks(np.arange(0, 45, 5))
         plt.yticks(np.arange(0.5, 1, 0.05))
         plt.rcParams['figure.figsize'] = (8, 6)
         plt.rcParams['figure.dpi'] = 600
@@ -29,13 +29,13 @@ class get_metric():
         plt.legend(['Training Acc.', 'Validation Acc.'], loc='lower right')
         plt.show()
 
-    def get_loss_graph(self, epochs, train_losses, val_losses):  # draw validation and train loss graphs
+    def get_loss_graph(epochs, train_losses, val_losses):  # draw validation and train loss graphs
         matplotlib.rcdefaults()
         plt.plot(epochs, train_losses, color='#006BA4')
         plt.plot(epochs, val_losses, color='#FF800E')
         plt.grid(b=True, which='major', color='lightgray')
         plt.grid(b=True, which='minor', color='lightgray')
-        plt.xticks(np.arange(0, 35, 5))
+        plt.xticks(np.arange(0, 45, 5))
         plt.yticks(np.arange(0, 1.2, 0.2))
         plt.rcParams['figure.dpi'] = 600
         plt.xlabel("Number of Epochs")
@@ -44,7 +44,7 @@ class get_metric():
         plt.legend(['Training Loss', 'Validation Loss'], loc='lower right')
         plt.show()
 
-    def test_label_predictions(self, model, device, test_loader):  # calculate outputs on test dataset for get metrics
+    def test_label_predictions(model, device, test_loader):  # calculate outputs on test dataset for get metrics
         model.eval()
         actuals = []
         predictions = []
@@ -56,27 +56,137 @@ class get_metric():
                 actuals.extend(target.view_as(prediction))
                 predictions.extend(prediction)
         return [i.item() for i in actuals], [i.item() for i in predictions]
+    
+    def test_label_predictions_el2(model_0,model_1,model_2,model_3, device, test_loader):
+    
+        actuals = []
+        predictions = []
 
-    def get_classification_report(self, model, device,
-                                  data):  # create classification report for each class with scikit-learn library
-        truth, predict = self.test_label_predictions(model, device, data)
+        with torch.no_grad():
+
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+
+                outputs_0 = model_0(data)
+                _, predicted_0 =torch.max(outputs_0.data, 1) 
+
+                outputs_1 = model_1(data)
+                _, predicted_1 =torch.max(outputs_1.data, 1) 
+
+                outputs_2 = model_2(data)
+                _, predicted_2 =torch.max(outputs_2.data, 1)
+
+                outputs_3 = model_3(data)
+                _, predicted_3 =torch.max(outputs_3.data, 1)
+
+                final_pred=predicted_1
+                size=final_pred.size()
+
+                for i in range(0,(size[0])):   
+                    a=0              
+                    if predicted_2[i].item()==0 and predicted_3[i].item()==0:
+
+                        if predicted_1[i].item()==1:
+                            final_pred[i]=1
+
+                        if predicted_1[i].item()==0:
+                            final_pred[i]=0
+                        a+=1
+
+                    if (predicted_0[i].item()==1 and predicted_1[i].item()==1) :
+
+                        a+=1   
+                        if predicted_3[i].item()==0:
+                            final_pred[i]=0 
+
+                        if predicted_3[i].item()!=0:
+                            final_pred[i]=1                    
+                    if a==0:                   
+                        final_pred[i]=predicted_2[i] 
+                actuals.extend(target.view_as(final_pred))
+                predictions.extend(final_pred)
+        return [i.item() for i in actuals], [i.item() for i in predictions]
+
+    def test_model(model ,device, test_loader):
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in test_loader:
+                images, labels = data[0].to(device), data[1].to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print('Correct Prediction: {:d}  Total Images: {:d}'.format(correct, total))
+        print('Test Accuracy = {:f}'.format(correct / total))
+    
+    def test_model_el2(model_0,model_1,model_2,model_3,device, test_loader):
+        
+        correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for data in test_loader:
+
+                images, labels = data[0].to(device), data[1].to(device)
+
+                outputs_0 = model_0(images)
+                _, predicted_0 =torch.max(outputs_0.data, 1) 
+
+                outputs_1 = model_1(images)
+                _, predicted_1 =torch.max(outputs_1.data, 1) 
+
+                outputs_2 = model_2(images)
+                _, predicted_2 =torch.max(outputs_2.data, 1)
+
+                outputs_3 = model_3(images)
+                _, predicted_3 =torch.max(outputs_3.data, 1)
+
+                final_pred=predicted_1
+                size=final_pred.size()
+
+                for i in range(0,(size[0])):   
+                    a=0              
+                    if predicted_2[i].item()==0 and predicted_3[i].item()==0:
+
+                        if predicted_1[i].item()==1:
+                            final_pred[i]=1
+
+                        if predicted_1[i].item()==0:
+                            final_pred[i]=0
+                        a+=1
+
+                    if (predicted_0[i].item()==1 and predicted_1[i].item()==1):
+
+                        a+=1
+
+                        if predicted_3[i].item()==0:
+                            final_pred[i]=0                        
+                        if predicted_3[i].item()!=0:
+                            final_pred[i]=1
+                    if a==0:                   
+                        final_pred[i]=predicted_2[i]
+
+                total += labels.size(0)
+                correct += (final_pred == labels).sum().item()
+        print('Correct Prediction: {:d}  Total Images: {:d}'.format(correct, total))
+        print('Test Accuracy = {:f}'.format(correct / total))
+
+    def get_classification_report(truth, predict):  # create classification report for each class with scikit-learn library
         print('Classification Report :\n', classification_report(truth, predict))
 
-    def get_confusion_matrix(self, model, device,
-                             data):  # create confusion matrix for each class with scikit-learn library
+    def get_confusion_matrix(actuals, predictions):  # create confusion matrix for each class with scikit-learn library
         matplotlib.rcdefaults()
-        truth, predict = self.test_label_predictions(model, device, data)
-        cf_matrix = confusion_matrix(truth, predict)
-        print('Confusion matrix:\n', cf_matrix)
-        sns.heatmap(cf_matrix, annot=True, fmt='g', cmap='Blues')
+        print('Confusion matrix:\n',confusion_matrix(actuals, predictions))
+        cf_matrix=confusion_matrix(actuals, predictions)
+        sns.heatmap(cf_matrix, annot=True,fmt='g', cmap='Blues')
 
-    def get_cohen_kappa(self, model, device, data):  # get cohen kapa score for   determine model performance
-        truth, predict = self.test_label_predictions(model, device, data)
-        cps = cohen_kappa_score(truth, predict)
+    def get_cohen_kappa(actuals, predictions):  # get cohen kapa score for   determine model performance
+        cps = cohen_kappa_score(actuals, predictions)
         print('Kappa Score of this model:\n', cps)
 
-    def test_class_probabilities(self, model, device, test_loader, which_class):
-        model.eval()
+    def test_class_probabilities(model, device, test_loader, which_class):
+        
         truths = []
         probabilities = []
         with torch.no_grad():
@@ -87,17 +197,104 @@ class get_metric():
                 truths.extend(target.view_as(prediction) == which_class)
                 probabilities.extend(np.exp(output[:, which_class]))
         return [i.item() for i in truths], [i.item() for i in probabilities]
+    def test_class_probabilities_el2(model_0,model_1,model_2,model_3, device, test_loader, which_class):
+    
+        truths = []
+        probabilities = []
 
-    def get_roc_curves(self, model, device, data):  # draw Roc curves and calculate auc score for each class
+        with torch.no_grad():
+            for data, target in test_loader:
+
+                data, target = data.to(device), target.to(device)
+
+                outputs_0 = model_0(data)
+                _, predicted_0 =torch.max(outputs_0.data, 1) 
+
+                outputs_1 = model_1(data)
+                _, predicted_1 =torch.max(outputs_1.data, 1) 
+
+                outputs_2 = model_2(data)
+                _, predicted_2 =torch.max(outputs_2.data, 1)
+
+                outputs_3 = model_3(data)
+                _, predicted_3 =torch.max(outputs_3.data, 1)
+
+                final_pred=predicted_1
+                out=outputs_1
+                size=final_pred.size()
+
+                for i in range(0,(size[0])):   
+                    a=0              
+                    if predicted_2[i].item()==0 and predicted_3[i].item()==0:
+
+                        if predicted_1[i].item()==1:
+                            #final_pred[i]=1
+                            out[i]=outputs_1[i]
+
+                        if predicted_1[i].item()==0:
+                            final_pred[i]=0
+                            out[i]=outputs_1[i]
+                        a+=1
+
+                    if (predicted_0[i].item()==1 and predicted_1[i].item()==1):
+
+                        a+=1
+
+                        if predicted_3[i].item()==0:
+                            #final_pred[i]=0
+                            out[i]=outputs_3[i]
+
+                        if predicted_3[i].item()!=0:
+                            #final_pred[i]=1
+                            out[i]=outputs_3[i]
+                    if a==0:                   
+                        #final_pred[i]=predicted_2[i]
+                        out[i]=outputs_2[i]
+                prediction = out.argmax(dim=1, keepdim=True)
+                truths.extend(target.view_as(prediction) == which_class)
+                probabilities.extend(np.exp(out.cuda().cpu()[:, which_class]))
+        return [i.item() for i in truths], [i.item() for i in probabilities]
+    
+    def get_roc_curves_el2(model_0,model_1,model_2,model_3, device, data):  # draw Roc curves and calculate auc score for each class
         fpr = dict()
         tpr = dict()
         roc_auc = dict()
 
-        actuals, class_probabilities = self.test_class_probabilities(model, device, data, 0)
+        actuals, class_probabilities = get_metric.test_class_probabilities_el2(model_0,model_1,model_2,model_3, device, data, 0)
         fpr[0], tpr[0], _ = roc_curve(actuals, class_probabilities)
         roc_auc[0] = roc_auc_score(actuals, class_probabilities)
 
-        actuals, class_probabilities = self.test_class_probabilities(model, device, data, 1)
+        actuals, class_probabilities = get_metric.test_class_probabilities_el2(model_0,model_1,model_2,model_3, device, data, 1)
+        fpr[1], tpr[1], _ = roc_curve(actuals, class_probabilities)
+        roc_auc[1] = roc_auc_score(actuals, class_probabilities)
+
+        print("Auc Score For Each Class: ", roc_auc)
+
+        matplotlib.rcdefaults()
+        plt.figure()
+        colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+        for i, color in zip(range(2), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=1,
+                     label='ROC curve of class {0} (area = {1:0.4f})'
+                           ''.format(i, roc_auc[i]))
+        plt.plot([0, 1], [0, 1], 'k--', lw=1)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.legend(loc="lower right")
+        plt.show()
+
+    def get_roc_curves(model, device, data):  # draw Roc curves and calculate auc score for each class
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+
+        actuals, class_probabilities = get_metric.test_class_probabilities(model, device, data, 0)
+        fpr[0], tpr[0], _ = roc_curve(actuals, class_probabilities)
+        roc_auc[0] = roc_auc_score(actuals, class_probabilities)
+
+        actuals, class_probabilities = get_metric.test_class_probabilities(model, device, data, 1)
         fpr[1], tpr[1], _ = roc_curve(actuals, class_probabilities)
         roc_auc[1] = roc_auc_score(actuals, class_probabilities)
 
